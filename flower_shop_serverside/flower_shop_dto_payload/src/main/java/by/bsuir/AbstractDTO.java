e4162876c83ff00b88031cee425a2b059f31e788
Ilya <ilya_chekun@mail.ru>
2020-10-31 12:43:56 +0300

    added dto module; fixed poms;

---
+++ flower_shop_serverside/flower_shop_dto_payload/src/main/java/by/bsuir/AbstractDTO.java
@@ -0,0 +1,21 @@
+package by.bsuir;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@EqualsAndHashCode(callSuper = true)
+public abstract class AbstractDTO extends BaseAbstractDTO {
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd hh.mm")
+    private LocalDateTime dateOfCreation;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd hh.mm")
+    private LocalDateTime dateOfLastUpdate;
+
+}
---
+++ flower_shop_serverside/flower_shop_dto_payload/src/main/java/by/bsuir/BaseAbstractDTO.java
@@ -0,0 +1,18 @@
+package by.bsuir;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+public class BaseAbstractDTO implements Serializable {
+
+    private Long id;
+
+}
