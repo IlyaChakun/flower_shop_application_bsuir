9128e21502aaac6a63fa4f1f96b921a0a26733eb
Ilya <ilya_chekun@mail.ru>
2020-11-04 00:28:57 +0300

    added controllers; fixed imports

---
+++ flower_shop_serverside/flower_shop_web/src/main/java/by/bsuir/controller/FlowerController.java
@@ -0,0 +1,19 @@
+package by.bsuir.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user/admin/company/shops/{id}/flowers")
+@AllArgsConstructor
+public class FlowerController {
+
+
+//    @GetMapping("/{id}")
+//
+//    @PostMapping
+//
+//    @PutMapping("/{id}")
+//
+//    @DeleteMapping("/{id}")
+}
---
+++ flower_shop_serverside/flower_shop_web/src/main/java/by/bsuir/controller/ShopController.java
@@ -0,0 +1,20 @@
+package by.bsuir.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user/admin/company/shops")
+@AllArgsConstructor
+public class ShopController {
+
+
+//    @GetMapping("/{id}")
+//
+//    @PostMapping()
+//
+//    @PutMapping("/{id}")
+//
+//    @DeleteMapping("/{id}")
+}
