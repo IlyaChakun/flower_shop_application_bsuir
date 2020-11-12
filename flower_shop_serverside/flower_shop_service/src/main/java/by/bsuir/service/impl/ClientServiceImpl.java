df124d5060f7a9bbb2539b718e73fec5e4d7b89f
Yuri Lukashevich <yurikluk@gmail.com>
2020-11-12 09:57:59 +0300

    serverside: client add update profile to controller

--- flower_shop_serverside/flower_shop_service/src/main/java/by/bsuir/service/impl/ClientServiceImpl.java
+++ flower_shop_serverside/flower_shop_service/src/main/java/by/bsuir/service/impl/ClientServiceImpl.java
@@ -2,12 +2,17 @@ package by.bsuir.service.impl;
 
 import by.bsuir.dto.mapper.user.ClientMapperDTO;
 import by.bsuir.dto.model.user.ClientDTO;
+import by.bsuir.entity.company.Company;
 import by.bsuir.entity.user.Client;
 import by.bsuir.payload.exception.ResourceNotFoundException;
+import by.bsuir.payload.exception.ServiceException;
 import by.bsuir.repository.api.user.ClientRepository;
 import by.bsuir.service.api.ClientService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @Service
 @AllArgsConstructor
@@ -32,5 +37,22 @@ public class ClientServiceImpl implements ClientService {
     @Override
     public Boolean existsByEmail(String email) {
         return clientRepository.existsByEmail(email);
+    }
+
+    @Override
+    @Transactional
+    public ClientDTO update(ClientDTO clientDTO, String email) {
+
+        Client client = clientRepository.findByEmail(email)
+                .orElseThrow(() -> {
+                    throw new ServiceException(HttpStatus.NOT_FOUND.value(),
+                            "client_not_found",
+                            "Client with email=" + email + " doesn't exist!");
+                });
+
+        Client clientToSave = clientMapper.toEntity(clientDTO);
+        clientToSave.setId(client.getId());
+
+        return clientMapper.toDto(clientRepository.save(clientToSave));
     }
 }
