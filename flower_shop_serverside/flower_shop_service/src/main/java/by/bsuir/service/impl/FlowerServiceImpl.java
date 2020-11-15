50a2799eb28a48aaabd702427cd15b5dff035c10
Yuri Lukashevich <yurikluk@gmail.com>
2020-11-16 01:41:53 +0300

    serverSide: need to fix FlowerServiceImpl

--- flower_shop_serverside/flower_shop_service/src/main/java/by/bsuir/service/impl/FlowerServiceImpl.java
+++ flower_shop_serverside/flower_shop_service/src/main/java/by/bsuir/service/impl/FlowerServiceImpl.java
@@ -3,12 +3,22 @@ package by.bsuir.service.impl;
 import by.bsuir.dto.mapper.product.FlowerMapperDTO;
 import by.bsuir.dto.model.PageWrapper;
 import by.bsuir.dto.model.SearchAndSortParamDto;
+import by.bsuir.dto.model.common.CountryDTO;
+import by.bsuir.dto.model.product.common.FlowerColorDTO;
+import by.bsuir.dto.model.product.common.FlowerSortDTO;
 import by.bsuir.dto.model.product.flower.FlowerDTO;
+import by.bsuir.entity.common.Country;
+import by.bsuir.entity.product.common.FlowerColor;
+import by.bsuir.entity.product.common.FlowerSort;
 import by.bsuir.entity.product.flower.Flower;
 import by.bsuir.payload.exception.ResourceNotFoundException;
 import by.bsuir.payload.exception.ServiceException;
+import by.bsuir.repository.api.core.CountryRepository;
+import by.bsuir.repository.api.core.FlowerColorRepository;
 import by.bsuir.repository.api.core.FlowerRepository;
+import by.bsuir.repository.api.core.FlowerSortRepository;
 import by.bsuir.service.api.FlowerService;
+import by.bsuir.service.api.ProductCommonService;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,6 +29,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -28,6 +40,12 @@ public class FlowerServiceImpl implements FlowerService {
 
     private final FlowerRepository flowerRepository;
     private final FlowerMapperDTO flowerMapper;
+    private final ProductCommonService productCommonService;
+
+    private final FlowerColorRepository flowerColorRepository;
+    private final FlowerSortRepository flowerSortRepository;
+    private final CountryRepository countryRepository;
+
 
     @Override
     public FlowerDTO findById(Long id) {
@@ -53,12 +71,46 @@ public class FlowerServiceImpl implements FlowerService {
 
 
         Flower flower = flowerMapper.toEntity(flowerDTO);
+        flower.setFlowerColors(resolveFlowerColors(flowerDTO.getFlowerColors()));
+        flower.setFlowerSorts(resolveFlowerSorts(flowerDTO.getFlowerSorts()));
+        flower.setCountry(resolveCountry(flowerDTO.getCountry()));
+
+        //TODO проверяем все цвета , если ид есть ок, если нет, то добавляем в базу и возвращаем ид
+        //TODO проверяем все сорта , если ид есть ок, если нет, то добавляем в базу и возвращаем ид
+        //TODO проверяем страну , если ид есть ок, если нет, то эксепшен
+
+        //TODO  в базу хардкодом пишем все цвета, все страны, сорта
 
         Flower savedFlower = flowerRepository.save(flower);
 
         logger.info("Saved Flower with id={} and type={}", flower.getId(), flower.getFlowerType());
 
         return flowerMapper.toDto(savedFlower);
+    }
+
+    private List<FlowerColor> resolveFlowerColors(List<FlowerColorDTO> flowerColorDTOList) {
+
+        return flowerColorDTOList.stream()
+                .map(flowerColorDTO -> flowerColorRepository.findById(flowerColorDTO.getId())
+                        .orElseGet(() -> flowerColorRepository.save(new FlowerColor(flowerColorDTO.getColorName()))))
+                .collect(Collectors.toList());
+    }
+
+    private List<FlowerSort> resolveFlowerSorts(List<FlowerSortDTO> flowerSortDTOList) {
+
+        return flowerSortDTOList.stream()
+                .map(flowerSortDTO -> flowerSortRepository.findById(flowerSortDTO.getId())
+                        .orElseGet(() -> flowerSortRepository.save(new FlowerSort(flowerSortDTO.getSortNameRu(), flowerSortDTO.getSortNameEn()))))
+                .collect(Collectors.toList());
+    }
+
+
+    private Country resolveCountry(CountryDTO countryDTO) {
+        return countryRepository.findById(countryDTO.getId())
+                .orElseThrow(() -> {
+                    logger.error("Country with id={} doesn't exist!", countryDTO.getId());
+                    throw new ResourceNotFoundException("Country with id=" + countryDTO.getId() + " not found!");
+                });
     }
 
     @Override
