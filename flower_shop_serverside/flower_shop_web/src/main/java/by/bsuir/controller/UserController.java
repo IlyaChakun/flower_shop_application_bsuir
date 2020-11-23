fcf2bcd087b8982e9d0638f1fe0ffef14dec2e36
Yuri Lukashevich <yurikluk@gmail.com>
2020-11-23 23:55:43 +0300

    serverSide: need to implement profile update

--- flower_shop_serverside/flower_shop_web/src/main/java/by/bsuir/controller/UserController.java
+++ flower_shop_serverside/flower_shop_web/src/main/java/by/bsuir/controller/UserController.java
@@ -1,17 +1,24 @@
 package by.bsuir.controller;
 
+import by.bsuir.dto.model.user.AbstractUserDTO;
+import by.bsuir.dto.model.user.ClientDTO;
+import by.bsuir.dto.model.user.ShopAdminDTO;
+import by.bsuir.dto.validation.annotation.PositiveLong;
 import by.bsuir.security.core.CurrentUser;
 import by.bsuir.security.core.UserPrincipal;
 import by.bsuir.security.dto.ApiResponse;
+import by.bsuir.security.dto.AuthTokenResponse;
 import by.bsuir.service.api.ClientService;
 import by.bsuir.service.api.ShopAdminService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+import static by.bsuir.controller.ControllerHelper.checkBindingResultAndThrowExceptionIfInvalid;
 
 @RestController
 @RequestMapping("/users")
@@ -25,9 +32,6 @@ public class UserController {
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/me")
     public ResponseEntity<?> me(@CurrentUser UserPrincipal userPrincipal) {
-
-        System.out.println("get me works");
-
         final String userEmail = userPrincipal.getEmail();
 
         if (clientService.existsByEmail(userEmail)) {
@@ -41,6 +45,29 @@ public class UserController {
         //courier
 
         return ResponseEntity.ok(new ApiResponse(false, "user does not exist!"));
+    }
+
+
+    @PostMapping("/{id}")
+    public ResponseEntity<AbstractUserDTO> updateProfile(@PathVariable("id") @PositiveLong String id,
+                                                         @CurrentUser UserPrincipal userPrincipal,
+                                                         @Valid AbstractUserDTO userDTO,
+                                                         BindingResult bindingResult) {
+        checkBindingResultAndThrowExceptionIfInvalid(bindingResult);
+
+        final String userEmail = userPrincipal.getEmail();
+
+        AbstractUserDTO user = null;
+
+        if (clientService.existsByEmail(userEmail)) {
+            user = clientService.update((ClientDTO) userDTO, id);
+        }
+
+//        if (shopAdminService.existsByEmail(userEmail)) {
+//            user = shopAdminService.update((ShopAdminDTO) userDTO);
+//        }
+
+        return ResponseEntity.ok(user);
     }
 
 }
