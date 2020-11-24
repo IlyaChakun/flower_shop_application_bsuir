d55d95753cccc90973a0d05ef956a1dd231ef863
Yuri Lukashevich <yurikluk@gmail.com>
2020-11-24 09:01:32 +0300

    serverSide: add update for profile - doesn.t work

--- flower_shop_serverside/flower_shop_service/src/main/java/by/bsuir/service/api/ShopAdminService.java
+++ flower_shop_serverside/flower_shop_service/src/main/java/by/bsuir/service/api/ShopAdminService.java
@@ -1,8 +1,9 @@
 package by.bsuir.service.api;
 
+import by.bsuir.dto.model.user.AbstractUserDTO;
 import by.bsuir.dto.model.user.ShopAdminDTO;
 
 public interface ShopAdminService extends AbstractUserService<ShopAdminDTO> {
 
-//    ShopAdminDTO update(ShopAdminDTO userDTO);
+    ShopAdminDTO update(AbstractUserDTO userDTO);
 }
--- flower_shop_serverside/flower_shop_service/src/main/java/by/bsuir/service/impl/ShopAdminServiceImpl.java
+++ flower_shop_serverside/flower_shop_service/src/main/java/by/bsuir/service/impl/ShopAdminServiceImpl.java
@@ -1,15 +1,22 @@
 package by.bsuir.service.impl;
 
 import by.bsuir.dto.mapper.user.ShopAdminMapperDTO;
+import by.bsuir.dto.model.user.AbstractUserDTO;
+import by.bsuir.dto.model.user.ClientDTO;
 import by.bsuir.dto.model.user.ShopAdminDTO;
+import by.bsuir.entity.user.Client;
 import by.bsuir.entity.user.ShopAdmin;
 import by.bsuir.payload.exception.ResourceNotFoundException;
+import by.bsuir.payload.exception.ServiceException;
 import by.bsuir.repository.api.user.ShopAdminRepository;
 import by.bsuir.service.api.ShopAdminService;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @Service
 @AllArgsConstructor
@@ -40,5 +47,17 @@ public class ShopAdminServiceImpl implements ShopAdminService {
     @Override
     public Boolean existsByEmail(String email) {
         return shopAdminRepository.existsByEmail(email);
+    }
+
+    @Override
+    @Transactional
+    public ShopAdminDTO update(AbstractUserDTO userDTO) {
+
+        ShopAdmin shopAdmin = shopAdminRepository.getByEmail(userDTO.getEmail());
+
+        shopAdmin.setName(userDTO.getName());
+        shopAdmin.setPhoneNumber(userDTO.getPhoneNumber());
+
+        return shopAdminMapper.toDto(shopAdmin);
     }
 }
