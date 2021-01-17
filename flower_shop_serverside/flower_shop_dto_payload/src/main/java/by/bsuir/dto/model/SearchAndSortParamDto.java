061020c243fc2d30323a97886611763824c56fa6
Ilya <ilya_chekun@mail.ru>
2021-01-17 12:43:31 +0300

    fixed todo

--- flower_shop_serverside/flower_shop_dto_payload/src/main/java/by/bsuir/dto/model/SearchAndSortParamDto.java
+++ flower_shop_serverside/flower_shop_dto_payload/src/main/java/by/bsuir/dto/model/SearchAndSortParamDto.java
@@ -1,5 +1,7 @@
 package by.bsuir.dto.model;
 
+import lombok.Data;
+
 import javax.validation.Valid;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
@@ -7,7 +9,8 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
-public class SearchAndSortParamDto extends AbstractDTO {//TODO а куда делся lombok?))
+@Data
+public class SearchAndSortParamDto extends AbstractDTO {
 
     @Size(min = 3, max = 30, message = "Name could not be empty! Should be not empty and in 4-30 characters range!")
     private String name;
@@ -30,56 +33,4 @@ public class SearchAndSortParamDto extends AbstractDTO {//TODO а куда де�
     @Pattern(regexp = "(asc|ASC|desc|DESC)", message = "sortType param could not be null or blank. Only ASC or DESC!")
     private String sortType;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public Set<String> getTags() {
-        return tags == null ? Collections.emptySet() : new HashSet<>(tags);
-    }
-
-    public void setTags(Set<String> tags) {
-        this.tags = tags == null ? Collections.emptySet() : new HashSet<>(tags);
-    }
-
-
-    public Set<String> getSortBy() {
-        return sortBy;
-    }
-
-    public void setSortBy(Set<String> sortBy) {
-        this.sortBy = sortBy;
-    }
-
-    public String getSortType() {
-        return sortType;
-    }
-
-    public void setSortType(String sortType) {
-        this.sortType = sortType;
-    }
-
-    @Override
-    public String toString() {
-        return "SearchAndSortParamDto{" +
-                super.toString() +
-                ", name='" + name + '\'' +
-                ", surname='" + surname + '\'' +
-                ", tags=" + tags +
-                ", sortBy=" + sortBy +
-                ", sortType='" + sortType + '\'' +
-                '}';
-    }
 }
