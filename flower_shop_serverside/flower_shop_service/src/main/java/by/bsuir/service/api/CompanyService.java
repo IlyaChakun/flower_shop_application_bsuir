24e7159dbdab3c7bc143af3d77a4dfc92dc40171
Yuriy_Lukashevich <yurikluk@gmail.com>
2021-01-17 17:57:32 +0300

    serverSide: add company service

---
+++ flower_shop_serverside/flower_shop_service/src/main/java/by/bsuir/service/api/CompanyService.java
@@ -0,0 +1,15 @@
+package by.bsuir.service.api;
+
+
+import by.bsuir.dto.model.company.CompanyDTO;
+import by.bsuir.service.core.base.FindOperationService;
+import by.bsuir.service.core.base.SaveOperationService;
+
+public interface CompanyService extends SaveOperationService<CompanyDTO>, FindOperationService<CompanyDTO> {
+
+    CompanyDTO findByName(String name);
+
+    CompanyDTO findByShopAdminId(Long adminId);
+
+    CompanyDTO update(Long companyId, Long id);
+}
