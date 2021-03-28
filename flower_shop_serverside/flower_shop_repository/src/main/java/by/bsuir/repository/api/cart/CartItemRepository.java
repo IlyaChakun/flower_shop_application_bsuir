7ff7bede964bf4e4c9792329d6a8206de8adeb20
Ilya <ilya_chekun@mail.ru>
2021-03-28 13:33:13 +0300

    fixed

--- flower_shop_serverside/flower_shop_repository/src/main/java/by/bsuir/repository/api/cart/CartItemRepository.java
+++ flower_shop_serverside/flower_shop_repository/src/main/java/by/bsuir/repository/api/cart/CartItemRepository.java
@@ -4,6 +4,11 @@ import by.bsuir.entity.cart.CartItem;
 import by.bsuir.repository.api.core.AbstractRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CartItemRepository extends AbstractRepository<CartItem> {
+
+    Optional<CartItem> findByClientIdAndProductIdAndProductLengthCostId(Long clientId, Long productId, Long productLengthCostId);
+
 }
