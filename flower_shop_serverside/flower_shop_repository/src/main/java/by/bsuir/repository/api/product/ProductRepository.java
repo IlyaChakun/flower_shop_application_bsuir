a8eb6bd2fb5ebf4a77fb1a370af0311f62157724
Ilya <ilya_chekun@mail.ru>
2021-04-18 20:25:16 +0300

    added product spec

--- flower_shop_serverside/flower_shop_repository/src/main/java/by/bsuir/repository/api/product/ProductRepository.java
+++ flower_shop_serverside/flower_shop_repository/src/main/java/by/bsuir/repository/api/product/ProductRepository.java
@@ -4,12 +4,13 @@ import by.bsuir.entity.product.Product;
 import by.bsuir.repository.api.core.AbstractRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends AbstractRepository<Product> {
 
-    Page<Product> findAllByIsDeletedFalse(Pageable pageable);
+    Page<Product> findAllByIsDeletedFalse(Specification specification, Pageable pageable);
 
     Page<Product> findAllByIsDeletedTrue(Pageable pageable);
 }
---
+++ flower_shop_serverside/flower_shop_repository/src/main/java/by/bsuir/repository/specification/ProductSpecification.java
@@ -0,0 +1,32 @@
+package by.bsuir.repository.specification;
+
+import by.bsuir.entity.order.Order;
+import by.bsuir.entity.product.Product;
+import org.springframework.data.jpa.domain.Specification;
+
+public final class ProductSpecification {
+
+    private ProductSpecification() {
+
+    }
+
+    public static Specification<Product> findByProductTitleLike(String title) {
+        return (Specification<Product>) (root, query, criteriaBuilder) ->
+                criteriaBuilder
+                        .like(root.get("title"), '%' + title + '%');
+    }
+
+    public static Specification<Product> findCategoryId(Long categoryId) {
+        return (root, query, criteriaBuilder) ->
+                criteriaBuilder
+                        .equal(root.get("categoryId"), categoryId);
+    }
+
+    public static Specification<Product> findByIsDeleted() {
+        return (root, query, criteriaBuilder) ->
+                criteriaBuilder
+                        .equal(root.get("isDeleted"), false);
+    }
+
+
+}
